587f7e0104bce70316e80277e01cf31ea0346c60
Julian Jimenez <julian.j.jimenez@student.uncommonhs.org>
2017-03-23 21:46:00 +0000

    tinyturtle2

--- script.js
+++ script.js
@@ -12,7 +12,73 @@ right(90);
 forward(50);
 }
 
+function triangle(size){
+    right(120);
+    forward(size);
+    right(120);
+    forward(size);
+    right(120);
+    forward(size);
+}
+
+function rectangle(length, length2){
+    right(90);
+    forward(length);
+    right(90);
+    forward(length2);
+    right(90);
+    forward(length);
+    right(90);
+    forward(length2);
+}
+
+function house(){
+    right(90);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+    forward(50);
+    right(30);
+    forward(50);
+    right(120);
+    forward(50);
+    right(120);
+    forward(50);
+}
+    
+function pentagon(){
+    forward(25);
+    right(60);
+    forward(25);
+    right(60);
+    forward(25);
+    right(60);
+    forward(25);
+    right(90);
+    forward(43);
+    right(90);
+}    
+
+function hexagon(){
+    forward(50);
+    right(60);
+    forward(50);
+    right(60);
+    forward(50);
+    right(60);
+    forward(50);
+    right(60);
+    forward(50);
+    right(60);
+    forward(50);
+}
 // Type your function call below
+
+hexagon();
 
 
 stamp();
+
